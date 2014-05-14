e2503cbbf2c657bfb1af6e0e17db09d667fe929e
Kangie Chan <aries0420@gmail.com>
2014-05-14 13:36:18 -0700

    Yee haw!

--- luck2.rb
+++ luck2.rb
@@ -4,8 +4,8 @@ class Luck2
   include Robot
 
   def tick events
-    turn 4 if time > 3
-    turn_gun 5
+    turn 5 if time > 3
+    turn_gun 6
     accelerate 8
     fire 3 unless events['robot_scanned'].empty?
   end
