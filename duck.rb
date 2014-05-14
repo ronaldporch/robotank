3bb8488aca319c939470b231684a223f897a67c9
xdothackerx <xdothackerx@gmail.com>
2014-05-14 13:22:00 -0700

    better killkilla

--- duck.rb
+++ duck.rb
@@ -4,11 +4,22 @@ class Duck
   include Robot
 
   def tick events
+    move_random
+    aimbot
+  end
+
+  def move_random
+    accelerate -1 + rand(2)
+    turn -10 + rand(20)
+  end
+
+  def aimbot
     turn_radar 60
-    #turn_radar -60
-    turn_gun 30 if time < 3
-    accelerate 1
-    turn -2
-    fire 3 unless events['robot_scanned'].empty?
+    unless events['robot_scanned'].empty?
+     puts radar_heading
+     turn_gun radar_heading - gun_heading
+     #puts radar_heading - gun_heading
+     fire 3
+    end
   end
 end
