02df7bfc099ddf284b7ba360ab845121f1be02d5
Kangie Chan <aries0420@gmail.com>
2014-05-14 13:10:02 -0700

    My Luck2 is getting stronger\!

--- luck.rb
+++ luck.rb
@@ -5,7 +5,7 @@ class Luck
 
   def tick events
     turn 4
-    accelerate 8
-    fire 3 #unless events['robot_scanned'].empty?
+    accelerate 6
+    fire 5# unless events['robot_scanned'].empty?
   end
 end
--- luck2.rb
+++ luck2.rb
@@ -4,8 +4,9 @@ class Luck2
   include Robot
 
   def tick events
-    turn 4
-    accelerate 5
+    turn 4 if time > 3
+    turn_gun 5
+    accelerate 8
     fire 3 unless events['robot_scanned'].empty?
   end
 end
