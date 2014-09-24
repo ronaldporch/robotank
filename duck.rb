86ae1f08ca000bfaf462a56585f12ab4f3a36b99
Brook Riggio <brooke.riggio@gmail.com>
2014-09-24 01:16:46 -0700

    Revert duck to original duck.

--- duck.rb
+++ duck.rb
@@ -4,21 +4,7 @@ class Duck
   include Robot
 
   def tick events
-    size = 1 if time == 0
-    move_random
-    aimbot
-  end
-
-  def move_random
-    accelerate -1 + rand(2)
-    turn -5 + rand(10)
-  end
-
-  def aimbot
-    turn_radar 60
-    unless events['robot_scanned'].empty?
-     turn_gun radar_heading - gun_heading
-     fire 3
-    end
+    turn 2
+    fire 3 unless events['robot_scanned'].empty?
   end
 end
